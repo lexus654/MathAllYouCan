ff055ead0064ad606ae02ceb017dda387b108bac
lexus654 <ronald.laz@tup.edu.ph>
2021-10-23 15:56:47 +0800

    efe

---
+++ src/components/test1.js
@@ -0,0 +1,12 @@
+import "./test1.css";
+
+const test = function () {
+  return (
+    <div>
+      <h1 className="left">hello world</h1>
+      <p>Hello guys this is test 1</p>
+    </div>
+  );
+};
+
+export default test;
